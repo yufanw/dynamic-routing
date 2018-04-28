5369bbc6cb43d71b4ba0859b548854120baf9717
Yufan Wang <yufanwangcn@hotmail.com>
2018-04-27 17:08:33 -0700

    added react router DOM, fixed styling

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+
+  let imgStyle = {
+    marginTop: 50,
+    width: 500,
+    textAlign: 'center'
+  };
+
+  return (
+    <img style={imgStyle} src="https://images.pexels.com/photos/891679/pexels-photo-891679.jpeg?cs=srgb&dl=blank-business-clean-891679.jpg&fm=jpg" />
+  );
+};
+
+export default Home;
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Sidebar = () => {
+
+  return (
+    <div className="leftNavContainer">
+
+    </div>
+  );
+};
+
+export default Sidebar;
